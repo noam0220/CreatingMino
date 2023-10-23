f42b1fe007851bd952c672bfa38eb0d1294804d1
Koshimoto <koshimoto@soft-co.jp>
2023-10-23 17:05:29 +0900

    ミノオブジェクトのデータ構造の変更(Minoクラス、MinoListクラスの追加) それに伴う処理の修正

---
+++ CreatingMino/MinoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingMino {
+    internal class MinoList {
+        public int CellCount { get; }
+        public List<Mino> MinoCollection { get; }
+
+        public MinoList(int cellCount, List<Mino> minoCollection) {
+            CellCount = cellCount;
+            MinoCollection = minoCollection;
+        }
+    }
+}
--- CreatingMino/Program.cs
+++ CreatingMino/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,37 +11,100 @@ namespace CreatingMino {
         //縦が長くなるように出力する。
         //180°回転させたときに等しくなるミノ同士は同一のミノとみなし、どちらか一つのみを出力する。
         //　
-        //　■　■■　■■　■　　　■　■■　■
-        //　■　■　　　■　■■　■■　■■　■■
-        //　■　■　　　■　■　　■　　　　　　■
+        //　■　■ ■　■ ■　■ 　　　 ■　■ ■　■ 
+        //　■　■ 　　　 ■　■ ■　■ ■　■ ■　■ ■
+        //　■　■ 　　　 ■　■ 　　■　　　　　　■ 
         //　■、　　、　　、　　、　　、　　、
         //　(出力例)
 
         //今回の使用セル数
         const int N = 4;
 
-        //結果を保持
-        static List<(bool[,], int[])> mino4List = new List<(bool[,], int[])>();
-
         //
         //　■　■■
         //　■　■
         //　■、
         //
-        //元となるミノ、縦横サイズ
+        //元となるミノ、縦横サイズ(旧)
         static List<(bool[,], int[])> mino3List = new List<(bool[,], int[])> {
             (new bool[N - 1, N - 1] { { true, false, false }, { true, false, false }, { true, false, false } }, new int[] { 3, 1 }),
             (new bool[N - 1, N - 1] { { false, false, false }, { true, true, false }, { false, true, false } }, new int[] { 2, 2 }),
         };
 
-        //重複が無いか確かめる場所
+        //元となるミノ、縦横サイズ
+        static MinoList oMinoList = new MinoList(3, new List<Mino> {
+            new Mino(new List<int[]> { new int[] { 0, 0 }, new int[] { 1, 0 }, new int[] { 2, 0 } }),
+            new Mino(new List<int[]> { new int[] { 0, 0 }, new int[] { 0, 1 }, new int[] { 1, 0 } })
+        });
+
+        //結果を保持(旧)
+        static List<(bool[,], int[])> mino4List = new List<(bool[,], int[])>();
+
+        //結果を保持
+        static MinoList minoList = new MinoList(4, new List<Mino> { });
+
+        //重複が無いか確かめる場所(旧)
         static bool[,] testField = new bool[N + 1, N + 1];
 
-        //サイズの初期値
+        //サイズの初期値(旧)
         static int[] size = new int[2] { 1, 1 };
 
         static void Main(string[] args) {
 
+            foreach(var oMino in oMinoList.MinoCollection) {
+                var testCells = new List<int[]>();
+                var tmpCells  = new List<int[]>();
+                //あとで、奴に理由を聞く
+                //var tmpCell = new int[2];
+                //foreach(var cell in oMino.Cells) {
+                //    tmpCell[0] = cell[0] + 1; tmpCell[1] = cell[1] + 1;
+                //    testCells.Add(tmpCell);
+                //}
+                foreach(var cell in oMino.Cells) {
+                    testCells.Add(new int[] { cell[0] + 1, cell[1] + 1 });
+                    tmpCells .Add(new int[] { cell[0] + 1, cell[1] + 1 });
+                }
+
+                var tmpCell = new int[2];
+                var directionList = new string[] { "above", "right", "below", "left" };
+
+                foreach(var cell in testCells) {
+                    foreach(var direction in directionList) {
+                        switch(direction) {
+                            case "above": tmpCell[0] = cell[0] - 1; tmpCell[1] = cell[1]; break;
+                            case "right": tmpCell[0] = cell[0]; tmpCell[1] = cell[1] + 1; break;
+                            case "below": tmpCell[0] = cell[0] + 1; tmpCell[1] = cell[1]; break;
+                            case "left" : tmpCell[0] = cell[0]; tmpCell[1] = cell[1] - 1; break;
+                        }
+                        if(!testCells.Any(c => c.SequenceEqual(tmpCell))) {
+                            tmpCells = AddMino(tmpCells, tmpCell);
+                        }
+                    }
+                }
+            }
+
+            foreach(var mino in minoList.MinoCollection) {
+                //いずれ、セルの順番をイイ感じにするメソッドを追加しないと、ヤバいはず
+                //もっといいのを、探そう
+                //将来的に、横一列に出力させる？
+                int[] tmpCell = new int[2];
+                Mino printMino = mino;
+                for(int row = 0; row < mino.Height; row++) {
+                    for(int col = 0; col < mino.Width; col++) {
+                        tmpCell[0] = row; tmpCell[1] = col;
+                        if(printMino.Cells.Any(c => c.SequenceEqual(tmpCell))) {
+                            Console.Write("■");
+                            printMino.Cells.Remove(tmpCell);
+                        } else {
+                            Console.Write("　");
+                        }
+                    }
+                    Console.Write("\n");
+                }
+                Console.Write("\n");
+            }
+
+            //旧
             foreach(var mino3 in mino3List) {
                 for(int row = 0; row < mino3.Item1.GetLength(0); row++) {
                     for(int col = 0; col < mino3.Item1.GetLength(1); col++) {
@@ -82,13 +146,14 @@ namespace CreatingMino {
                 size[0] = 1; size[1] = 1;
             }
 
+            //旧
             foreach(var mino4 in mino4List) {
                 for(int row = 0; row < mino4.Item1.GetLength(0); row++) {
                     for(int col = 0; col < mino4.Item1.GetLength(1); col++) {
                         if(mino4.Item1[row, col]) {
-                            Console.Write("■");
+                            //Console.Write("■");
                         } else {
-                            Console.Write("　");
+                            //Console.Write("　");
                         }
                     }
                     Console.Write("\n");
@@ -97,6 +162,78 @@ namespace CreatingMino {
             }
         }
 
+        private static List<int[]> AddMino(List<int[]> cells, int[] tCell) { 
+            cells.Add(tCell);
+            cells = cells.OrderBy(x => x[0]).ThenBy(y => y[1]).ToList();
+
+            var cellsWithNoSpace = new List<int[]>();
+
+            int xMin = cells.Min(x => x[1]);
+            int yMin = cells.Min(y => y[0]);
+
+            foreach(var cell in cells) {
+                cellsWithNoSpace.Add(new int[] { cell[0] - yMin, cell[1] - xMin });
+            }
+
+            int width = cellsWithNoSpace.Max(y => y[1]) - cellsWithNoSpace.Min(y => y[1]) + 1;
+            int height = cellsWithNoSpace.Max(y => y[0]) - cellsWithNoSpace.Min(y => y[0]) + 1;
+
+            if(height < width) {
+                //縦より横の方が長い場合、図形を時計回りに90°回転させる
+                var tmpCells = new List<int[]>();
+                var tmpCellsWithNoSpace = new List<int[]>();
+                foreach(var cell in cellsWithNoSpace) {
+                    tmpCells.Add(new int[] { cell[1], height - cell[0] });
+                }
+                tmpCells = tmpCells.OrderBy(x => x[0]).ThenBy(y => y[1]).ToList();
+
+                xMin = tmpCells.Min(x => x[1]);
+                yMin = tmpCells.Min(y => y[0]);
+
+                foreach(var cell in tmpCells) {
+                    tmpCellsWithNoSpace.Add(new int[] { cell[0] - yMin, cell[1] - xMin });
+                }
+
+                cellsWithNoSpace = tmpCellsWithNoSpace;
+                (height, width) = (width, height);
+            }
+
+            if(CheckDuplication2(cellsWithNoSpace, height, width)) {
+                minoList.MinoCollection.Add(new Mino(cellsWithNoSpace));
+            };  
+            cells.Remove(tCell);
+            return cells;
+        }
+
+        private static bool CheckDuplication2(List<int[]> cells, int height, int width) {
+            foreach(var mino in minoList.MinoCollection) {
+                if(height == mino.Height && width == mino.Width) {
+                    //同じかどうか比較
+                    bool sameFlg = true;
+                    for(int i = 0; i < mino.Cells.Count; i++) {
+                        if(!Enumerable.SequenceEqual(cells[i], mino.Cells[i])) { sameFlg = false; }
+                    }
+                    if(sameFlg) { return false; }
+                    if(Enumerable.SequenceEqual(cells, mino.Cells)) { return false; }
+                    //180°回転させて、同じかどうか比較
+                    var tmpCells = new List<int[]>{ };
+                    //ミスあり
+                    foreach(var cell in cells) {
+                        tmpCells.Add(new int[] { height - cell[0] - 1, width - cell[1] - 1 });
+                    }
+
+                    tmpCells = tmpCells.OrderBy(x => x[0]).ThenBy(y => y[1]).ToList();
+                    sameFlg = true;
+                    for(int i = 0; i < mino.Cells.Count; i++){
+                        if(!Enumerable.SequenceEqual(tmpCells[i], mino.Cells[i])) { sameFlg = false; }
+                    }
+                    if(sameFlg) { return false; }
+                }
+            } 
+            return true;
+        }
+
+        //旧
         private static void CheckDuplication(bool[,] field, int[] size) {
             List<int[]> cellList = new List<int[]>();
 
